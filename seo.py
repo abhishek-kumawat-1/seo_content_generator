e1f31213184a137365a444fb8e106c775b5ab143
Abhishek Kumawat <112508513+abhishek-kumawat-1@users.noreply.github.com>
2025-11-03 12:05:03 +0530

    langchain.text_splitter deprecated

--- seo.py
+++ seo.py
@@ -263,7 +263,8 @@ from bs4 import BeautifulSoup
 
    # New (recommended)
 from langchain_core.documents import Document 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
